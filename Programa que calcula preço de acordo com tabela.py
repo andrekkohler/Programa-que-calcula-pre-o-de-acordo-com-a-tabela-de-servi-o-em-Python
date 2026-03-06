d01d0e1befbb5d8429d16285cf65517e68eda5b7
andrekkohler <andrekkohler@gmail.com>
2026-03-06 20:21:34 -0300

    Add files via upload

---
+++ Programa que calcula preço de acordo com tabela.py
@@ -0,0 +1,26 @@
+kwh = float(input("Informe a quantidade de kWh: "))
+tipo = input("Qual o tipo da instalação? (R, C ou I): ")
+
+if(tipo == "R"):
+    if kwh >= 500:
+        preco = 0.65
+    else:
+        preco = 0.4
+        print(f"Total a pagar: {kwh * preco}")
+
+elif(tipo == "C"):
+    if kwh >= 1000:
+        preco = 0.6
+    else:
+        preco = 0.55
+        print(f"Total a pagar: {kwh * preco}")
+
+elif(tipo == "I"):
+    if kwh >= 5000:
+        preco = 0.6
+    else:
+        preco = 0.55
+        print(f"Total a pagar: {kwh * preco}")
+
+else:
+    print("Instalação Inválida. Encerrando...")
